41022b45c4edcb41264eebc67b1d70d470fce060
Ishita <ishitagops12@gmail.com>
2022-02-15 17:38:02 +0530

    deletion of table content

---
+++ delete.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+conn = sqlite3.connect('students.db')
+
+conn.execute("DELETE from STUDENT where ID = 2;")
+conn.commit()
+
+cursor = conn.execute("SELECT * from STUDENT")
+print(cursor.fetchall())
+
+conn.close()
